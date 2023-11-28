459d99df3081709f5bb54d245e54d9402da1ea66
meysamzi <meysamzeynali2@gmail.com>
2023-11-28 23:14:11 +0330

    feat: adding change check box handler to users table

--- src/Components/Form/user-autocomplete.tsx
+++ src/Components/Form/user-autocomplete.tsx
@@ -1,15 +1,25 @@
-import { AutoComplete, Button, Checkbox, Popover } from 'antd';
-import { EllipsisOutlined } from '@ant-design/icons'
+import { useState, useEffect } from 'react'
+import { AutoComplete, Button, Checkbox } from 'antd';
 import { NodeType } from '../../types';
 
 interface Props {
   initialValue?: NodeType;
 }
 
+interface IUser {
+  title: string
+  isDefault: boolean
+}
+
 const UserAutoComplete = ({ initialValue }: Props) => {
+  const [selectedUsers, setSelectedUsers] = useState([])
   const onSelect = (data: string) => {
     console.log('onSelect', data);
   };
+
+  useEffect(() => {
+    if (initialValue) setSelectedUsers(initialValue.users)
+  }, [initialValue])
 
   const renderTitle = (title: string) => (
     <span>
@@ -18,6 +28,15 @@ const UserAutoComplete = ({ initialValue }: Props) => {
   );
 
   const usersData = initialValue?.users?.map((data: any) => { return { label: renderTitle(data?.title) } })
+
+  const onChangeCheckBox = (user: IUser) => {
+    if (!user.isDefault) {
+      const users = selectedUsers.map(x => {
+        return { ...x, isDefault: x.title === user.title ? true : false }
+      })
+      setSelectedUsers(users)
+    }
+  }
 
   return (
     <>
@@ -29,16 +48,16 @@ const UserAutoComplete = ({ initialValue }: Props) => {
       />
       <Button >افزودن</Button>
       <div style={{ marginTop: '24px' }}>
-        {initialValue?.users?.length > 0 && <table className='usersTable'>
+        {selectedUsers?.length > 0 && <table className='usersTable'>
           <tbody>
             <tr>
               {["عملیات", "پیش فرض", "نام"].map(header => <th key={header}>{header}</th>)}
             </tr>
-            {initialValue?.users?.map((user: any, index: number) =>
+            {selectedUsers?.map((user: any, index: number) =>
               <tr key={index}>
                 <td align='center'>
                 </td>
-                <td align='center'><Checkbox checked={user.isDefault} /></td>
+                <td align='center'><Checkbox checked={user.isDefault} onChange={() => onChangeCheckBox(user)} /></td>
                 <td align='center'>{user.title}</td>
               </tr>
             )}
