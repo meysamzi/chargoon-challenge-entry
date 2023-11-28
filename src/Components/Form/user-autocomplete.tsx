02bc079c3acfa27d6677e3f9b75b3b72f008a265
meysamzi <meysamzeynali2@gmail.com>
2023-11-28 19:16:07 +0330

    feat: adding users table to the left section forms

--- src/Components/Form/user-autocomplete.tsx
+++ src/Components/Form/user-autocomplete.tsx
@@ -1,4 +1,5 @@
-import { AutoComplete, Button } from 'antd';
+import { AutoComplete, Button, Checkbox, Popover } from 'antd';
+import { EllipsisOutlined } from '@ant-design/icons'
 import { NodeType } from '../../types';
 
 interface Props {
@@ -27,6 +28,23 @@ const UserAutoComplete = ({ initialValue }: Props) => {
         placeholder="جستجوی کاربر"
       />
       <Button >افزودن</Button>
+      <div style={{ marginTop: '24px' }}>
+        {initialValue?.users?.length > 0 && <table className='usersTable'>
+          <tbody>
+            <tr>
+              {["عملیات", "پیش فرض", "نام"].map(header => <th key={header}>{header}</th>)}
+            </tr>
+            {initialValue?.users?.map((user: any, index: number) =>
+              <tr key={index}>
+                <td align='center'>
+                </td>
+                <td align='center'><Checkbox checked={user.isDefault} /></td>
+                <td align='center'>{user.title}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>}
+      </ div>
     </>
   );
 };
